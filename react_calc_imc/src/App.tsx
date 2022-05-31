fe556288fa3142e0ae970a1b3b3a872069cc7ca1
gabriel1og <gogomide.go@gmail.com>
2022-05-31 17:31:41 -0300

    disabled e responsividade

--- react_calc_imc/src/App.tsx
+++ react_calc_imc/src/App.tsx
@@ -44,6 +44,7 @@ const App = () => {
              placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
              value={heightField > 0 ? heightField : ''}
              onChange={e => setHeightField(parseFloat(e.target.value))}
+             disabled={toShow ? true : false}
             />
 
             <input
@@ -51,9 +52,10 @@ const App = () => {
              placeholder='Digite o seu peso. Ex: 75.3 (em kg)'
              value={weightField > 0 ? weightField : ''}
              onChange={e => setWeightField(parseFloat(e.target.value))}
+             disabled={toShow ? true : false}
             />
 
-            <button onClick={handleCalculateButton}>Calcular</button>
+            <button onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
           </div>
           <div className={styles.rightSide}>
             {!toShow &&
